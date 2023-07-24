e95242481224ef7aaba63a2533a11979dc24c827
HuniaNadeem-DevIntern <hunia.intern@devsinc.com>
2023-07-24 22:24:19 +0500

    Comments added

--- input.rb
+++ input.rb
@@ -28,14 +28,17 @@ module Input
     exit
   end
 
+  # checks if correct format input is given at CLI
   def check_condition(input)
     !input[2].nil? && ((input[0] == '-e' && check_year_format(input[1])) ||
       ((input[0] == '-a' || input[0] == '-c') && check_month_format(input[1])))
   end
 
+  # checks format yyyy/month
   def check_month_format(date)
     if date.match?(%r{^\d{4}/\d{2}$}) || date.match?(%r{^\d{4}/\d{1}$})
       parts = date.split('/')
+      # checks if added month is within range
       return false if parts[1].to_i < 1 || parts[1].to_i > 12
 
       true
@@ -44,6 +47,7 @@ module Input
     end
   end
 
+  # checks format yyyy
   def check_year_format(date)
     date.match?(/^\d{4}$/)
   end
--- user_input.rb
+++ user_input.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'input'
-
 class UserInput
   include Input
 
@@ -11,11 +10,15 @@ class UserInput
   end
 
   def main
+    # correct input is being checked to continue with the program flow. Otherwise, code exits
     check_input(@input)
+    # input details are fetched separately
     fetch_input
+    # input details are returned as Hash object
     @query
   end
 
+  # stores CLI input values separately in a Hash object
   def fetch_input
     if @input[0] == '-e'
       @query[:query] = 1
@@ -36,6 +39,7 @@ class UserInput
 
   def fetch_month(mode = '')
     months = %w[jan feb mar apr may jun jul aug sep oct nov dec]
+    # if mode is 'i', it returns month number. Otherwise, returns month string
     return months.find_index(@input[:month]) if mode.casecmp('i').zero?
 
     month_value = @input[1].split('/')[1].to_i
